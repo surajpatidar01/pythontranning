a25d2f1d51eb86579563f65a95f1f4431b559c44
surajpatidar01 <ptdrsuraj@gmail.com>
2025-02-25 20:39:28 +0530

    vowel eater program

--- Assignments/1-ArithmaticOperators/arithmetic.py
+++ Assignments/1-ArithmaticOperators/arithmetic.py
@@ -820,6 +820,15 @@ else:
 
 
 
+'''You must design the (ugly) vowel eater! Write a program that uses:
+a for loop;
+the concept of conditional execution (if-elif-else)
+the continue statement.
+Your program must:
+ask the user to enter a word;
+use user_word = user_word.upper() to convert the word entered by the user to upper case; we'll talk about string methods and the upper() method very soon - don't worry;
+use conditional execution and the continue statement to "eat" the following vowels A, E, I, O, U from the inputted word;
+assign the uneaten letters to the word_without_vowels variable and print the variable to the screen.'''
 
 
 '''word = input(" please Enter a word: ")
