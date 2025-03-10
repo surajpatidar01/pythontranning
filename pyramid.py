253c57d28bce3219e744be7a3ec1e5e8245efe56
surajpatidar01 <ptdrsuraj@gmail.com>
2025-03-10 16:01:52 +0530

    pyramid

--- pyramid.py
+++ pyramid.py
@@ -9,7 +9,7 @@ the builders have, and outputs the height of the pyramid that can
 be built using these blocks.
 Note: the height is measured by the number of fully completed layers –
 if the builders don't have a sufficient number of blocks and cannot
-complete the next layer, they finish their work immediately.
+complete the next layer, they finish their work immediately.'''
 
 
 blocks=int(input("Enter the number of blocks:"))
@@ -20,4 +20,4 @@ while   used_blocks +(height +1) <= blocks:
     height +=  1
     used_blocks += height
 
-print("The height of the pyramid:", height)'''
+print("The height of the pyramid:", height)
