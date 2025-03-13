2c648071bd207c40ef834423d84419477ed82035
surajpatidar01 <ptdrsuraj@gmail.com>
2025-03-13 10:12:47 +0530

    assignment qus

--- assignment.py
+++ assignment.py
@@ -1,7 +1,7 @@
 #Q1--Write a program to print the first 10 natural numbers using for loop.
 
 #for i in range(1,11):
-#   print(i)'''
+#   print(i)
 
 #Q2--Write a program to print all the even numbers within the range of 10
 
